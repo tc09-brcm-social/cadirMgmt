0079746d2708b6db7e1abaf723a1e4bd4cf6e43b
Tommy Cheng <yihhwa.cheng@broadcom.com>
2021-11-19 17:39:02 +0000

    file: file_upload checks curl version

--- file/file_upload.sh
+++ file/file_upload.sh
@@ -5,5 +5,11 @@ if [[ -z "$LDIF" ]] ; then
     >&2 echo "LDIF file required"
     exit 1
 fi
+. "$MYPATH/env.shlib"
+VER=$(bash utils/curlver.sh)
+if [[ "$VER" < "$TESTEDCURL" ]]; then
+    >&2 echo "$0 requies curl version newer than $TESTEDCURL"
+    exit 1
+fi
 ADDL="--header,Accept: application/json,-H,Content-Type: multipart/form-data,-F,uploadedFile=@${LDIF}"
 bash "${MYPATH}/file_upload/coreZ00/post.sh" "" "$ADDL"
